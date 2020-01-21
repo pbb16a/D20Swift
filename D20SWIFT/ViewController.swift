13f71131c6d0c7ba62e797eb0731ed783b5817e0
Paula Berggren <pbb16a@acu.edu>
2020-01-21 14:15:22 -0600

    1st commit

---
+++ D20SWIFT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  D20SWIFT
+//
+//  Created by Paula Berggren on 1/21/20.
+//  Copyright © 2020 Paula Berggren. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ D20SWIFTTests/D20SWIFTTests.swift
@@ -0,0 +1,34 @@
+//
+//  D20SWIFTTests.swift
+//  D20SWIFTTests
+//
+//  Created by Paula Berggren on 1/21/20.
+//  Copyright © 2020 Paula Berggren. All rights reserved.
+//
+
+import XCTest
+@testable import D20SWIFT
+
+class D20SWIFTTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
