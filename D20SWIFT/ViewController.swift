124511da43a855072e7d611fe7f43e18129154bc
Paula Berggren <pbb16a@acu.edu>
2020-01-21 14:57:50 -0600

    working

--- D20SWIFT/ViewController.swift
+++ D20SWIFT/ViewController.swift
@@ -8,13 +8,28 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func rollDice(){
+        let rolledNumber = Int.random(in: 1...20)
+        
+        let imageName = "d\(rolledNumber)"
+        
+        diceImageView.image = UIImage(named: imageName)
+        
+    }
 
+    @IBAction func rollDiceFromButton(){
+        rollDice()
+    }
 }
 
