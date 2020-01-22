77bbf20ab1d00453e443803c0b25a365e7f35abb
Paula Berggren <pbb16a@acu.edu>
2020-01-21 19:01:41 -0600

    Sounds are working

--- D20SWIFT/ViewController.swift
+++ D20SWIFT/ViewController.swift
@@ -5,30 +5,75 @@
 //  Created by Paula Berggren on 1/21/20.
 //  Copyright © 2020 Paula Berggren. All rights reserved.
 //
-
 import UIKit
+import AVFoundation
 
-
-
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate{
+    var rollSound: AVAudioPlayer?
+    
+   
+    
     @IBOutlet weak var diceImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
+   }
     func rollDice(){
+        //play roll sound
+        let path = Bundle.main.path(forResource: "rolldice", ofType:"mp3")!
+        let url = URL(fileURLWithPath: path)
+        do {
+            rollSound = try AVAudioPlayer(contentsOf: url)
+            rollSound?.play()
+        } catch {
+        }
+        
         let rolledNumber = Int.random(in: 1...20)
         
-        let imageName = "d\(rolledNumber)"
+        var imageName = "d\(rolledNumber)"
         
+        if(rolledNumber == 7){
+            let rolledNumber2 = Int.random(in: 1...2)
+            if(rolledNumber2 == 2){
+                imageName = "d7cruiser"
+                let path = Bundle.main.path(forResource: "torpedo", ofType:"mp3")!
+                let url = URL(fileURLWithPath: path)
+                do {
+                    rollSound = try AVAudioPlayer(contentsOf: url)
+                    rollSound?.play()
+                } catch {
+                }
+            }
+        }
+    
         diceImageView.image = UIImage(named: imageName)
         
+        if(rolledNumber == 1){
+            let path = Bundle.main.path(forResource: "failwah", ofType:"mp3")!
+            let url = URL(fileURLWithPath: path)
+            do {
+                rollSound = try AVAudioPlayer(contentsOf: url)
+                rollSound?.play()
+            } catch {
+            }
+        }
+        if(rolledNumber == 20){
+            //play roll sound
+            let path = Bundle.main.path(forResource: "zfanfare", ofType:"mp3")!
+            let url = URL(fileURLWithPath: path)
+            do {
+                rollSound = try AVAudioPlayer(contentsOf: url)
+                rollSound?.play()
+            } catch {
+            }
+        }
     }
 
     @IBAction func rollDiceFromButton(){
+        rollDice()
+    }
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         rollDice()
     }
 }
